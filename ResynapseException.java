8b7c4443c3335294ff046d955be4a33d9676d0d1
Cengiz Gunay <cengique@users.sf.net>
2001-03-13 03:31:08 +0000

    Empty exception, intact.

---
+++ ResynapseException.java
@@ -0,0 +1,18 @@
+
+/**
+ * ResynapseException.java
+ * Exception thrown in case two synaptic boutons of an axon end up
+ * on the same postsynaptic neuroid.
+ *
+ * Created: Sun Mar 11 01:42:59 2001
+ *
+ * @author Cengiz Gunay
+ * @version
+ */
+
+public class ResynapseException extends Exception {
+    public ResynapseException (String msg) {
+	super(msg);
+    }
+    
+}// ResynapseException
