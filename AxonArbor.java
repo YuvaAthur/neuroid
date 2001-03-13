1804f0fd337247329519bb37336b54bb345492df
Cengiz Gunay <cengique@users.sf.net>
2001-03-13 02:56:42 +0000

    Group outgoing connections in an AxonArbor. Area.createRandomSynapses retries if one axon attempts to make multiple synapses on same destination neuron. AxonArbor throws ResynapseException in this case.

---
+++ AxonArbor.java
@@ -0,0 +1,56 @@
+import java.lang.*;
+import java.util.*;
+import java.rmi.*;
+import Utils.*;
+
+/**
+ * AxonArbor.java
+ *
+ *
+ * Created: Thu Mar  8 16:43:37 2001
+ *
+ * @author Cengiz Gunay
+ * @version
+ */
+
+public class AxonArbor extends Vector {
+    public AxonArbor () {
+	super();
+    }
+
+    public AxonArbor (int initialCapacity) {
+	super(initialCapacity);
+    }
+    
+    /**
+     * Adds a synapse to list of synapses
+     * only if it doesn't already exist.
+     *
+     * @param synapse a <code>Synapse</code> value
+     * @exception ResynapseException if the <code>synapse</code> is already in arbor.
+     */
+    public void addSynapse(Synapse synapse) throws ResynapseException {
+
+	// Check all existing synapses for the same postsynaptic neuron
+	Object[] p = { synapse, new Boolean(false) };
+	Iteration.loop(iterator(), new Utils.TaskWithParam() { 
+		public void job(Object o, Object[] p) {
+		    Synapse existingSynapse = (Synapse) o;
+		    Synapse newSynapse = (Synapse) p[0];
+
+		    if (existingSynapse.getDestNeuroid().equals(newSynapse.getDestNeuroid())) {
+			p[1] = new Boolean(true);
+		    } // end of if
+
+		}}, p);
+
+	
+	/*if (((Boolean)p[1]).booleanValue()) 
+	    // throw exception if neuroid found in existing synapses
+	    throw new ResynapseException("Synapsing on same neuron, try again!");
+	else */
+	    // otherwise accept new synapse
+	    add(synapse);
+    }
+    
+}// AxonArbor
