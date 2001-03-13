78be2b00d83ed027b0090b4b43fc33e87a0dcbdd
Cengiz Gunay <cengique@users.sf.net>
2001-03-13 03:27:12 +0000

    Two areas: input and circuit. Two concepts and balancing number of noise concepts in input area.

---
+++ MultiConceptPeripheral.java
@@ -0,0 +1,133 @@
+import java.util.*;
+import java.rmi.*;
+import Utils.*;
+
+/**
+ * Two areas: input and circuit.
+ * Two concepts and balancing number of noise concepts in input area.
+ *
+ * Created: Mon Dec 11 02:52:07 2000
+ *
+ * @author Cengiz Gunay
+ * @version
+ */
+
+public class MultiConceptPeripheral extends Peripheral {
+    Concept a, b;
+    Vector noiseConcepts;
+    Object inputArea, circuitArea;
+
+    public MultiConceptPeripheral (Network network, Area inputArea, Area circuitArea) {
+	super(network);
+	this.inputArea = inputArea;
+	this.circuitArea = circuitArea;
+
+	allocateConcepts();
+    }
+
+    public MultiConceptPeripheral (Network network, RemoteAreaInt inputArea, 
+				   RemoteAreaInt circuitArea) {
+	super(network);
+	this.inputArea = inputArea;
+	this.circuitArea = circuitArea;
+	
+	allocateConcepts();
+    }
+
+    void allocateConcepts() {
+	if (inputArea instanceof Area) {
+	    a = new Concept((Area)inputArea);
+	    b = new Concept((Area)inputArea);
+	} else {
+	    a = new Concept((RemoteAreaInt)inputArea);
+	    b = new Concept((RemoteAreaInt)inputArea);
+	} // end of else
+
+	int inputReplication, destReplication, destNumberOfNeuroids;
+
+	if (circuitArea instanceof Area) {
+	    inputReplication = ((Area)inputArea).getReplication();
+	    destReplication = ((Area)circuitArea).getReplication();
+	    destNumberOfNeuroids = ((Area)circuitArea).getNumberOfNeuroids();
+	} else {
+	    try {
+		inputReplication = ((RemoteAreaInt)inputArea).getReplication();
+		destReplication = ((RemoteAreaInt)circuitArea).getReplication();
+		destNumberOfNeuroids = ((RemoteAreaInt)circuitArea).getNumberOfNeuroids(); 
+	    } catch (RemoteException e) {
+		System.out.println("Cannot call RemoteArea methods.");
+		e.printStackTrace();
+		return;
+	    }
+	} // end of else	
+	
+	// calculate remaining concepts needed to equalize weights between areas
+	int remainingConcepts =
+	    Math.max(0, (int)Math.ceil((2*Math.sqrt(destReplication * destNumberOfNeuroids)
+					- destReplication) /
+				       inputReplication)
+		     - 2);
+	System.out.println("Allocating " + remainingConcepts +
+			   " more Concepts in inputArea to balance load...");
+
+	noiseConcepts = new Vector();
+	for (int i = 0; i < remainingConcepts; i++) {
+	    if (inputArea instanceof Area) {
+		noiseConcepts.add(new Concept((Area)inputArea));
+	    } else {
+		noiseConcepts.add(new Concept((RemoteAreaInt)inputArea));
+	    } // end of else
+	    
+	} // end of for (int i = 0; i < remainingConcepts; i++)
+    }
+
+    public void fireInputs() {
+	a.fire();
+	b.fire();
+    }
+
+    public void testOneInput() {
+	a.fire();
+    }
+
+    public void fireRandomNoise() {
+	Iteration.loop(noiseConcepts.iterator(), new Task() {
+		int i = 0;
+		public void job(Object o) {
+		    // 1/300 chance should make all of them fire once during 300 steps(!)
+		    int level = (int)(Math.random()*300);
+		    if (level == 1) {
+			((Concept) o).fire();
+			System.out.println("Firing random concept...");
+			
+		    } else 
+			return;
+		}
+	    });
+    }
+
+    public void fireNoise1() {
+	Iteration.loop(noiseConcepts.iterator(), new Task() {
+		int i = 0;
+		public void job(Object o) {
+		    if (i++ < noiseConcepts.size()/2) 
+			return;
+		    else 		    
+			((Concept) o).fire();
+		}
+	    });
+    }
+
+    public void fireNoise2() {
+	Iteration.loop(noiseConcepts.iterator(), new Task() {
+		int i = 0;
+		public void job(Object o) {
+		    if (i++ > noiseConcepts.size()/2) {
+			return;
+		    } // end of if (i++ > noiseConcepts.size()/2)
+		    
+		    ((Concept) o).fire();
+		}
+	    });
+    }
+}// MultiConceptPeripheral
