f3ebbcacba1d50d8981a63e28f90a0d6c9531924
Cengiz Gunay <cengique@users.sf.net>
2001-04-05 08:07:09 +0000

    Concept created by peripheral controller. Associated with a <code>SensoryNeuroid</code>

---
+++ periphery/SensoryConcept.java
@@ -0,0 +1,21 @@
+package periphery;
+import Base.*;
+// $Id$
+/**
+ * Concept created by peripheral controller. Associated with a <code>SensoryNeuroid</code>.
+ * @see Peripheral
+ * @see SensoryNeuroid
+ * 
+ * <p>Created: Sat Mar 31 22:37:28 2001
+ * <p>Modified: $Date$
+ *
+ * @author Cengiz Gunay
+ * @version $Revision$ for this file.
+ */
+
+public class SensoryConcept extends ArtificialConcept {
+    public SensoryConcept (Network network, String name) {
+	super(network, name);
+    }
+    
+}// SensoryConcept
