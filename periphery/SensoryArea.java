cba9c99e3f2a53df1b6913eb423dd89dfd256781
Cengiz Gunay <cengique@users.sf.net>
2001-04-05 08:04:49 +0000

    Area that holds sensoryneuroids. Adds itself automatically to the network.

---
+++ periphery/SensoryArea.java
@@ -0,0 +1,24 @@
+package periphery;
+
+// $Id$
+/**
+ * Area that holds inputs, ie. <code>SensoryNeuroid</code>s. This area should not be in
+ * the list of areas that the network contains.
+ * @see SensoryNeuroid
+ * @see SensoryConcept
+ *
+ * <p>Created: Mon Apr  2 00:32:27 2001
+ * <p>Modified: $Date$
+ *
+ * @author Cengiz Gunay
+ * @version $Revision$ for this file.
+ */
+
+public class SensoryArea extends Base.Area {
+    public SensoryArea (Base.Network network, String name) {
+	// threshold 0.9? get it from somewhere?
+	super(network, name, 0, 1, 0, 0.9, false); 
+	network.addArea(this);	// TODO: add areas to network automatically? what about remoteareas?
+    }
+    
+}// SensoryArea
