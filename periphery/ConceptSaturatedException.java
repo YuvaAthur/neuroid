0b70ff83899a917fff4ab4527239934dda05ec73
Cengiz Gunay <cengique@users.sf.net>
2001-09-22 19:36:07 +0000

    @see ArtificialConcept#attach

---
+++ periphery/ConceptSaturatedException.java
@@ -0,0 +1,25 @@
+package Base;
+import Base.*;
+import Remote.*;
+
+// $Id$
+/**
+ * ConceptSaturatedException.java
+ * Exception thrown from ArtificialConcept.attach() whenever a Neuroid is
+ * recruited tries to attach to a Concept which has already Area.replication
+ * members.
+ *
+ * <p>Created: Tue Sep 18 23:24:35 2001
+ * <p>Modified: $Date$
+ *
+ * @author Cengiz Gunay
+ * @version $Revision$ for this file.
+ * @see ArtificialConcept.attach
+ */
+
+public class ConceptSaturatedException extends Exception {
+    public ConceptSaturatedException (String msg) {
+	super(msg);
+    }
+
+}
