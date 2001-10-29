ca87c9a5338e69e1d2641eeef1625044f8756ecc
Cengiz Gunay <cengique@users.sf.net>
2001-10-29 07:11:54 +0000

    initial.

---
+++ ntr/NameNotFoundException.java
@@ -0,0 +1,21 @@
+package Base;
+import Base.*;
+import Remote.*;
+
+// $Id$
+/**
+ * NameNotFoundException.java
+ *
+ *
+ * <p>Created: Sun Oct 28 22:25:07 2001
+ * <p>Modified: $Date$
+ *
+ * @author Cengiz Gunay
+ * @version $Revision$ for this file.
+ */
+
+public class NameNotFoundException extends Exception {
+    public NameNotFoundException (String msg) {
+	super(msg);
+    }
+}
