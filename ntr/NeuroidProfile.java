cdef554c5a70b06349c1f4a4fcf5b4ce49cc8d2c
Cengiz Gunay <cengique@users.sf.net>
2002-04-15 23:22:13 +0000

    Moved from package ntr.plots to ntr in order to keep plots application independent. Now uses subclass ModeProfile of Profile to keep track of mode changes. get/set methods added. Synapse profiles are created and added to the Vector. Properly sets the title of the getSpikesEmittedPlot (name change)

--- ntr/NeuroidProfile.java
+++ ntr/NeuroidProfile.java
@@ -1,14 +1,15 @@
 
-package neuroidnet.ntr.plots;
+package neuroidnet.ntr;
 
-import neuroidnet.ntr.*;
+import neuroidnet.utils.*;
+import neuroidnet.ntr.plots.*;
 
 import java.io.*;
 import java.util.*;
 
 // $Id$
 /**
- * NeuroidProfile.java
+ * Keeping track of changes of <code>Neuroid</code>s.
  *
  *
  * <p>Created: Tue Apr  9 15:25:36 2002
@@ -20,13 +21,13 @@ import java.util.*;
 
 public class NeuroidProfile implements Serializable {
 
-    Profile modeProfile;
+    ModeProfile modeProfile;
     
     /**
      * Get the value of modeProfile.
      * @return value of modeProfile.
      */
-    public Profile getModeProfile() {
+    public ModeProfile getModeProfile() {
 	return modeProfile;
     }
     
@@ -34,26 +35,74 @@ public class NeuroidProfile implements Serializable {
      * Set the value of modeProfile.
      * @param v  Value to assign to modeProfile.
      */
-    public void setModeProfile(Profile  v) {
+    public void setModeProfile(ModeProfile  v) {
 	this.modeProfile = v;
     }
-    
 
-    Vector synapseProfiles;
+    final Vector synapseProfiles = new Vector();
+    
+    /**
+     * Get the value of synapseProfiles.
+     * @return value of synapseProfiles.
+     */
+    public Vector getSynapseProfiles() {
+	return synapseProfiles;
+    }
+    
     Neuroid neuroid;
-    public Vector spikesEmitted = new Vector();
+    
+    /**
+     * Get the value of neuroid.
+     * @return value of neuroid.
+     */
+    public Neuroid getNeuroid() {
+	return neuroid;
+    }
+    
+    /**
+     * Set the value of neuroid.
+     * @param v  Value to assign to neuroid.
+     */
+    public void setNeuroid(Neuroid  v) {
+	this.neuroid = v;
+    }
+
+    Vector spikesEmitted = new Vector();
+    
+    /**
+     * Get the value of spikesEmitted.
+     * @return value of spikesEmitted.
+     */
+    public Vector getSpikesEmitted() {
+	return spikesEmitted;
+    }
+    
+    /**
+     * Set the value of spikesEmitted.
+     * @param v  Value to assign to spikesEmitted.
+     */
+    public void setSpikesEmitted(Vector  v) {
+	this.spikesEmitted = v;
+    }
 
     public NeuroidProfile (Neuroid neuroid) {
 	this.neuroid = neuroid;
 
 	// iterate on all synapses and set their watches, then take their profiles
+	Iteration.loop(neuroid.synapses, new Task() {
+		public void job(Object o) {
+		    Synapse s = ((Synapse)o);
+		    s.setWatch(true);
+		    synapseProfiles.add(s.getProfile());
+		}
+	    });
 	
 	// create a new modeProfile here.
-	modeProfile = new Profile(neuroid.getMode(), new Double(neuroid.getArea().time));
+	modeProfile = new ModeProfile(neuroid.getMode(), new Double(neuroid.getArea().time));
     }
 
-    public SpikePlot spikesEmittedPlot() {
-	return new SpikePlot(null, null, spikesEmitted);
+    public SpikePlot getSpikesEmittedPlot() {
+	return new SpikePlot("Spikes emitted from " + neuroid, null, spikesEmitted);
     }
 
 }// NeuroidProfile
