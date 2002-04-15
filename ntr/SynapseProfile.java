c59f325bba9839585264815d1f869b967267f2ce
Cengiz Gunay <cengique@users.sf.net>
2002-04-15 23:28:53 +0000

    Initial version. Keeps a weight profile and can create weight and spikesReceived plots.

---
+++ ntr/SynapseProfile.java
@@ -0,0 +1,73 @@
+
+package neuroidnet.ntr;
+
+import neuroidnet.utils.*;
+import neuroidnet.ntr.plots.*;
+
+import java.io.*;
+
+// $Id$
+/**
+ * SynapseProfile.java
+ *
+ *
+ * <p>Created: Sun Apr 14 00:10:02 2002
+ * <p>Modified: $Date$
+ *
+ * @author <a href="mailto:">Cengiz Gunay</a>
+ * @version $Revision$ for this file.
+ */
+
+public class SynapseProfile  implements Serializable {
+    Profile weightProfile;
+    
+    /**
+     * Get the value of weightProfile.
+     * @return value of weightProfile.
+     */
+    public Profile getWeightProfile() {
+	return weightProfile;
+    }
+    
+    /**
+     * Set the value of weightProfile.
+     * @param v  Value to assign to weightProfile.
+     */
+    public void setWeightProfile(Profile  v) {
+	this.weightProfile = v;
+    }
+    
+    Synapse synapse;
+    
+    /**
+     * Get the value of synapse.
+     * @return value of synapse.
+     */
+    public Synapse getSynapse() {
+	return synapse;
+    }
+    
+    /**
+     * Set the value of synapse.
+     * @param v  Value to assign to synapse.
+     */
+    public void setSynapse(Synapse  v) {
+	this.synapse = v;
+    }
+
+    public SynapseProfile (Synapse synapse) {
+	this.synapse = synapse;
+
+	weightProfile = new Profile(synapse.getWeightObject(),
+				     new Double(synapse.destNeuroid.getArea().time));
+    }
+
+    public SpikePlot getSpikesReceivedPlot() {
+	return new SpikePlot("Spikes received from " + synapse, null, synapse.getSpikeTrain());
+    }
+
+    public Plot getWeightPlot() {
+	return new ProfilePlot("Weight of " + synapse, null, getWeightProfile());
+    }
+}
+    
