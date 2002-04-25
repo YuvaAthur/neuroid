876227fc5faadd9fa6454422a8d15e7556b324ea
Cengiz Gunay <cengique@users.sf.net>
2002-04-25 03:21:55 +0000

    Initial, works.

---
+++ ntr/PotentialPlot.java
@@ -0,0 +1,85 @@
+
+package neuroidnet.ntr;
+
+import neuroidnet.utils.StringTask;
+import neuroidnet.ntr.plots.*;
+
+import java.util.*;
+
+// $Id$
+/**
+ * Plot for synapse potential.
+ *
+ *
+ * <p>Created: Mon Apr 15 22:15:15 2002
+ * <p>Modified: $Date$
+ *
+ * @see Synapse
+ * @author <a href="mailto:cengiz@ull.edu">Cengiz Gunay</a>
+ * @version $Revision$ for this file.
+ */
+
+public class PotentialPlot extends Plot  {
+    SynapseProfile synapseProfile;
+
+    public PotentialPlot (String title, Range range, SynapseProfile synapseProfile) {
+	super(title, range);
+
+	this.synapseProfile = synapseProfile;
+
+	try {
+	    if (range == null) {
+		Synapse synapse = synapseProfile.getSynapse();
+		Vector spikes = synapse.getSpikeTrain();
+		this.range = new Range(((Double)spikes.firstElement()).doubleValue(),
+				       ((Double)spikes.lastElement()).doubleValue() +
+				       synapse.getTimeConstantM() + synapse.getTimeConstantS());
+	    } // end of if (range == null)
+
+	    // expand the range with profile changes?
+	    //range.add(synapseProfile.getRange());
+	} catch (NoSuchElementException e) {
+	    // do nothing, keep empty range
+	} // end of try-catch
+	
+    }
+
+    /**
+     * Define the <code>epsp()</code> function here.
+     *
+     * @return a <code>String</code> value
+     */
+    public String preamble() {
+	String time = sub("t","delay");
+	String[] params = {"t", "timeConstantM", "timeConstantS", "delay"};
+	return def_func("epsp", params,
+			mul(geq(time, "0"),
+			    div(sub(exp(neg(div(time, "timeConstantM"))),
+				    exp(neg(div(time, "timeConstantS")))),
+				sub("1", div("timeConstantS", "timeConstantM")))));
+    }
+
+    /**
+     * Iterates on the spikes receive times of the synapse and adds
+     * <code>epsp()</code> functions for each.
+     * @see #preamble
+     * @return a <code>String</code> value
+     */
+    public String body() {
+	Synapse synapse = synapseProfile.getSynapse();
+	final String
+	    timeConstantM = "" + synapse.getTimeConstantM(),
+	    timeConstantS = "" + synapse.getTimeConstantS(), 
+	    delay =  "" + synapse.getDelay();
+
+	// iterate on spikes in synapse and add them up
+	return new StringTask("0") {
+		public void job(Object o) {
+		    String[] params = { sub("t", "" + o), timeConstantM,
+					timeConstantS, delay};
+		    retval = add(retval, func("epsp", params));
+		}
+	    }.getString(synapse.getSpikeTrain());
+    }
+    
+}// PotentialPlot
---
+++ ntr/WeightedPotentialPlot.java
@@ -0,0 +1,32 @@
+
+package neuroidnet.ntr;
+
+import neuroidnet.ntr.plots.*;
+
+// $Id$
+/**
+ * Weighted potential plot of a synapse.
+ *
+ *
+ * <p>Created: Tue Apr 16 14:15:32 2002
+ * <p>Modified: $Date$
+ *
+ * @author <a href="mailto:">Cengiz Gunay</a>
+ * @version $Revision$ for this file.
+ */
+
+public class WeightedPotentialPlot extends PotentialPlot  {
+    public WeightedPotentialPlot (String title, Range range, SynapseProfile synapseProfile) {
+	super(title, range, synapseProfile);
+
+	if (this.range == null) 
+	    // get range from the profile
+	    this.range = synapseProfile.getWeightProfile().getRange();
+	//TODO: otherwise add range from profile?
+    }
+
+    public String body() {
+	return mul(profile(synapseProfile.getWeightProfile(), range), super.body());
+    }
+    
+}// WeightedPotentialPlot
