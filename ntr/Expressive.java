34166fa8ad0ca88aacd138ca0e733f7a1fceb952
Cengiz Gunay <cengique@users.sf.net>
2002-05-02 23:55:35 +0000

    Removed neuroidnet.ntr.plots and neuroidnet.utils packages to edu.ull.cgunay.utils. New interface Expressive to dictate cleaner conversion to strings for output and plots.

---
+++ ntr/Expressive.java
@@ -0,0 +1,39 @@
+
+package neuroidnet.ntr;
+
+// $Id$
+/**
+ * Defines an interface on how an entity should express its identity and state.
+ *
+ * <p>Created: Wed May  1 16:38:56 2002
+ * <p>Modified: $Date$
+ *
+ * @author <a href="mailto:cengiz@ull.edu">Cengiz Gunay</a>
+ * @version $Revision$ for this file.
+ */
+
+public interface Expressive  {
+    /**
+     * Give enough information on the entity for identification.
+     *
+     * @return a <code>String</code> value
+     */
+    String toString();
+
+    /**
+     * In addition to <code>toString()</code> contents,
+     * give transient properties of the entity without
+     * the static properties.
+     *
+     * @return a <code>String</code> value
+     */
+    String getStatus();
+
+    /**
+     * In addition to <code>getProperties()</code> contents,
+     * give all static properties also.
+     *
+     * @return a <code>String</code> value
+     */
+    String getProperties();
+}// Expressive
